91bb621a3c7a4713f93d8336a17735e0e580a7be
Yabish Arfath <yabisharfath7@gmail.com>
2021-09-16 15:04:41 +0400

    messages ongoing

--- lib/screens/messages/components/audio_message.dart
+++ lib/screens/messages/components/audio_message.dart
@@ -1,5 +1,6 @@
 import 'package:chat/constants.dart';
 import 'package:chat/models/ChatMessage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class AudioMessage extends StatelessWidget {
@@ -25,7 +26,45 @@ class AudioMessage extends StatelessWidget {
             Icons.play_arrow,
             color: message.isSender ? Colors.white : kPrimaryColor,
           ),
-          // Expanded(child: child)
+          Expanded(
+            child: Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: kDefaultPadding / 2),
+              child: Stack(
+                clipBehavior: Clip.none,
+                alignment: Alignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 2,
+                    decoration: BoxDecoration(
+                      color: message.isSender
+                          ? Colors.white
+                          : kPrimaryColor.withOpacity(0.4),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  Positioned(
+                    left: 0,
+                    child: Container(
+                      height: kDefaultPadding / 2,
+                      width: kDefaultPadding / 2,
+                      decoration: BoxDecoration(
+                        color: kPrimaryColor,
+                        shape: BoxShape.circle,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Text(
+            '0:37',
+            style: TextStyle(
+              fontSize: 12,
+            ),
+          ),
         ],
       ),
     );
